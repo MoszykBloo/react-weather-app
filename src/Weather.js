8b8e16fa56b076ab5fec2f1365dac9682ec41c34
MoszykBloo <monica.camacho.cadena@gmail.com>
2020-10-27 17:16:58 -0600

    playing with react

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,20 @@
 import React from "react";
+import './App.css';
 
 export default function Weather(){
     return (
-        <h2>My first React App</h2>
+        <div>
+            <h2>My first React App</h2>
+            <small>
+                <a
+                    href="https://github.com/MoszykBloo/react-weather-app"
+                    target="blank"
+                    id="credits"
+                >
+                    Open source code
+                </a>{" "}
+          <span id="my-name">by Monica Camacho</span>
+        </small>
+        </div>
     )
 }
